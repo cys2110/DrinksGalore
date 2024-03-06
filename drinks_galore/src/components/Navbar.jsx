f7ddb66daa3957ffeed5886bc3a304e1da906f68
VenomSymbiotes <156369944+VenomSymbiotes@users.noreply.github.com>
2024-03-06 18:02:13 -0500

    Some basic CSS so far

--- drinks_galore/src/components/Navbar.jsx
+++ drinks_galore/src/components/Navbar.jsx
@@ -1,16 +1,24 @@
- // Import the Link component from react-router-dom
- import { Link } from 'react-router-dom';
-    
-    const Nav = () => {
-      return (
-        <nav className="navbar">        
-          <div>
-            {/* Links to navigate to different routes */}
-            <Link to="/drinks" className="nav-link">Drinks</Link>
-            <Link to="/ingredients" className="nav-link">Ingredients</Link>
-          </div>
-        </nav>
-      )
-    }
-    
-    export default Nav;
+// Import the Link component from react-router-dom
+import { Link } from "react-router-dom";
+import "../App.css";
+
+const Nav = () => {
+  return (
+    <nav className="navbar">
+      <div>
+        {/* Links to navigate to different routes */}
+        <Link to="/" className="nav-link">
+          Drinks Galore
+        </Link>
+        <Link to="/drinks" className="nav-link">
+          Drinks
+        </Link>
+        <Link to="/ingredients" className="nav-link">
+          Ingredients
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
